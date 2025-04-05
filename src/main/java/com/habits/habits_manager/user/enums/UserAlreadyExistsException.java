2bc0980f50b841d93b3533c1bf0f69d390811099
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2025-04-05 18:33:18 -0300

    feat(BACK-016): User ALready Exists Exception

---
+++ src/main/java/com/habits/habits_manager/user/enums/UserAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.habits.habits_manager.user.enums;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+  public UserAlreadyExistsException(MessageSource ms) {
+    super(ms.getMessage("error-messages.user-already-exists", null, LocaleContextHolder.getLocale()));
+  }
+}
