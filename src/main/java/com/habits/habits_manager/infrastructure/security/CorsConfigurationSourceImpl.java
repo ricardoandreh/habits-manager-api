6b9d4ee3593b1180dad7a93ab1fba36a25248a25
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2025-04-05 03:55:12 -0300

    feat(BACK-008): Adding CORS Configuration

---
+++ src/main/java/com/habits/habits_manager/infrastructure/security/CorsConfigurationSourceImpl.java
@@ -0,0 +1,24 @@
+package com.habits.habits_manager.infrastructure.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfigurationSourceImpl implements CorsConfigurationSource {
+    @Override
+    public CorsConfiguration getCorsConfiguration(@NonNull HttpServletRequest request) {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        return corsConfiguration;
+    }
+
+}
