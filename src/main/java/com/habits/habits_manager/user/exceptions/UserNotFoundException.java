b1ba08af3b7807eed3f692176cec97db8c49addf
ricardoandreh <ricardo.andre.ifc@gmail.com>
2025-04-10 01:44:57 -0300

    refactor: user DTO & findByEmail on /me endpoint method

--- src/main/java/com/habits/habits_manager/user/exceptions/UserNotFoundException.java
+++ src/main/java/com/habits/habits_manager/user/exceptions/UserNotFoundException.java
@@ -1,5 +1,6 @@
 package com.habits.habits_manager.user.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) { super(message);}
     public UserNotFoundException(Long id) { super("Resource not found. id:  " + id);}
 }
